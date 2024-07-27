d65beb50e7474d5e1728f0f0e6db19fe7c45dca2
Nikhil Linga <nikhil_linga@epam.com>
2024-07-27 14:33:03 +0530

    e3r3

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-function_validation_output=$(echo -e "❌ \033[0;31m Hello test \033[0m")
-
-echo  "function_validation_output=$function_validation_output" >> $GITHUB_OUTPUT
-
+function_validation_output=""
+function_validation_output+=$(echo -e "❌ \033[0;31m Hello test \033[0m")
+function_validation_output+=$(echo -e "❌ \033[0;31m Hello test 3 \033[0m")
+echo "function_validation_output=$function_validation_output" >> $GITHUB_OUTPUT
